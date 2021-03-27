de000f6160f77e780521e24cddcfb76a9e4aa6a8
displaceit <77670397+displaceit@users.noreply.github.com>
2021-03-27 12:24:28 +0100

    ver2703

--- script/color-menu.js
+++ script/color-menu.js
@@ -1,6 +1,8 @@
 const list = $('.colors-menu');
 list.on('click', '.colors-menu__line', function(e) {
     e.preventDefault()
-    $(this).siblings('li').removeClass('colors-menu__line_active').animate({width:'toggle'},500).delay(2000).animate();
+    $(this).siblings('li').removeClass('colors-menu__line_active').animate({width:'toggle'},500).delay(5000).animate();
     $(this).toggleClass('colors-menu__line_active');
     })
+
+    //
